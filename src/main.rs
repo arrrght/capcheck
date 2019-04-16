218f82348bca9c8819a552ea131b638945bd7398
a3r <arrrght@gmail.com>
2019-04-16 14:43:37 +0500

    prometheus stats

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
-extern crate reqwest;
+#[macro_use]
+extern crate log;
 
+use prometheus::{Counter, Encoder, IntGauge, Opts, Registry, TextEncoder};
 use reqwest::Client;
 use std::io::Read;
 use std::time::Instant;
 
+const CAP_URL: &str = "https://cap.avtocod.ru";
+//const CAP_URL: &str = "http://127.0.0.1";
+
 fn main() {
-    let paste_api = "http://127.0.0.1:8088/in.php";
-    let client = Client::new().post(paste_api);
+    env_logger::init();
+
+    let reg = Registry::new();
+    let retry_cnt = Counter::with_opts(Opts::new("retry_counter", "Retry counter")).unwrap();
+    let gauge = IntGauge::with_opts(Opts::new("access_time", "Access time to CapMonster")).unwrap();
+
+    reg.register(Box::new(retry_cnt.clone())).unwrap();
+    reg.register(Box::new(gauge.clone())).unwrap();
+
+    let client = Client::new().post(&format!("{}/in.php", CAP_URL));
     let form = reqwest::multipart::Form::new()
         .text("method", "post")
         .file("file", "generate.jpg")
@@ -18,17 +31,27 @@ fn main() {
     response.read_to_string(&mut response_body).unwrap();
     let ans: Vec<&str> = response_body.split("|").collect();
     let ans_int: usize = ans[1].to_string().parse().unwrap();
-    println!("ANS1:: {}", ans_int);
+    debug!("ANS1:: {}", ans_int);
 
     let mut ret: Option<String> = None;
     while ret.is_none() {
-        let check_api = format!("http://127.0.0.1:8088/res.php?action=get&id={}", ans_int);
+        retry_cnt.inc();
+        let check_api = format!("{}/res.php?action=get&id={}", CAP_URL, ans_int);
         ret = match reqwest::get(&check_api).unwrap().text().unwrap().as_ref() {
             "CAPCHA_NOT_READY" => None,
             x if x.starts_with("OK") => Some(x.to_string()),
-            _ => panic!("F>U>C>K")
+            _ => panic!("F>U>C>K"),
         };
     }
     let time_elapsed = time_now.elapsed().subsec_millis();
-    println!("ANS2:: {}, msec: {}", ret.unwrap(), time_elapsed);
+    gauge.set(time_elapsed as i64);
+    debug!("ANS2:: {}, msec: {}", ret.unwrap(), time_elapsed);
+
+    let mut buffer = vec![];
+    let encoder = TextEncoder::new();
+    let metric_families = reg.gather();
+    encoder.encode(&metric_families, &mut buffer).unwrap();
+
+    println!("{}", String::from_utf8(buffer).unwrap());
+
 }
