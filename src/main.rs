e5b140df773151616f71e779904eead725ad17a6
a3r <arrrght@gmail.com>
2019-04-18 20:43:40 +0500

    Added 100ms wait time between retries, compiled image in binary

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::io::Read;
 use std::time::Instant;
 
 const CAP_URL: &str = "https://cap.avtocod.ru";
-//const CAP_URL: &str = "http://127.0.0.1";
+//const CAP_URL: &str = "http://127.0.0.1:8088";
 
 fn main() {
     env_logger::init();
@@ -15,15 +15,20 @@ fn main() {
     let reg = Registry::new();
     let retry_cnt = Counter::with_opts(Opts::new("retry_counter", "Retry counter")).unwrap();
     let gauge = IntGauge::with_opts(Opts::new("access_time", "Access time to CapMonster")).unwrap();
+    let error = IntGauge::with_opts(Opts::new("error", "some error")).unwrap();
 
     reg.register(Box::new(retry_cnt.clone())).unwrap();
     reg.register(Box::new(gauge.clone())).unwrap();
+    reg.register(Box::new(error.clone())).unwrap();
 
     let client = Client::new().post(&format!("{}/in.php", CAP_URL));
+    let file_part = reqwest::multipart::Part::bytes(&include_bytes!("generate.jpg")[..])
+        .file_name("generate.jpg")
+        .mime_str("image/jpeg")
+        .unwrap();
     let form = reqwest::multipart::Form::new()
         .text("method", "post")
-        .file("file", "generate.jpg")
-        .unwrap();
+        .part("file", file_part);
 
     let time_now = Instant::now();
     let mut response = client.multipart(form).send().unwrap();
@@ -37,10 +42,14 @@ fn main() {
     while ret.is_none() {
         retry_cnt.inc();
         let check_api = format!("{}/res.php?action=get&id={}", CAP_URL, ans_int);
-        ret = match reqwest::get(&check_api).unwrap().text().unwrap().as_ref() {
-            "CAPCHA_NOT_READY" => None,
+        let req_get = reqwest::get(&check_api).unwrap().text();
+        ret = match req_get.unwrap().as_ref() {
+            "CAPCHA_NOT_READY" => {
+                std::thread::sleep(std::time::Duration::from_millis(100));
+                None 
+            },
             x if x.starts_with("OK") => Some(x.to_string()),
-            _ => panic!("F>U>C>K"),
+            x => panic!("F>U>C>K {:?}", x),
         };
     }
     let time_elapsed = time_now.elapsed().subsec_millis();
@@ -53,5 +62,4 @@ fn main() {
     encoder.encode(&metric_families, &mut buffer).unwrap();
 
     println!("{}", String::from_utf8(buffer).unwrap());
-
 }
